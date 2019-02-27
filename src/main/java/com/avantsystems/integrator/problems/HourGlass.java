2960c6acd51df0324c2ee008ab9582da6d95539b
Josh Uzo <nwankwo.joshua@gmail.com>
2019-02-27 21:22:48 +0800

    App development

--- src/main/java/com/avantsystems/integrator/problems/HourGlass.java
+++ src/main/java/com/avantsystems/integrator/problems/HourGlass.java
@@ -1,11 +1,13 @@
 package com.avantsystems.integrator.problems;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class HourGlass {
 
-    private static final int[][] matrix  = {
+    private static final int[][] MATRIX  = {
             {-9, -9, -9,  1, 1, 1},
             { 0, -9,  0,  4, 3, 2},
             {-9, -9, -9,  1, 2, 3},
@@ -15,24 +17,31 @@ public class HourGlass {
     };
 
     private List<Integer> sumList = new ArrayList<>();
+    private int maxIntVal = Integer.MIN_VALUE;
 
 
+    public void process(int[][] matrix ){
 
-    public void process(){
+        int prevVal = 0;
         for (int i = 0; i < 4; i++){
             for (int j = 0; j < 4; j++) {
                int sum =  matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] + matrix[i + 1][j + 1] + matrix[i + 2][j]
                        + matrix[i + 2][j + 1] + matrix[i + 2][j + 2];
 
+               if (sum > maxIntVal) {
+                   maxIntVal = sum;
+               }
                sumList.add(Integer.valueOf(sum));
-
             }
         }
         System.out.println(sumList.toString());
     }
 
     public static void main(String[] args) {
-        new HourGlass().process();
+        HourGlass hourGlass = new HourGlass();
+        hourGlass.process(HourGlass.MATRIX);
+        System.out.println(hourGlass.maxIntVal);
+        //Collections.max(hourGlass.sumList);
     }
 
 
