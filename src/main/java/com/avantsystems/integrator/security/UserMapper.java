8658705747d05354e42a1fc44af7987c8217fa86
joshua <nwankwo.joshua@gmail.com>
2019-03-08 16:45:10 +1100

    Refactoring: Inline temporary variable that is assigned once.

--- src/main/java/com/avantsystems/integrator/security/UserMapper.java
+++ src/main/java/com/avantsystems/integrator/security/UserMapper.java
@@ -5,16 +5,11 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserMapper  {
+public class UserMapper {
 
     protected static final String GET_USER_QUERY = "select user_id, user_name, encryted_password from avant_user";
 
     //this throws an SQL exception but test it first
-    RowMapper<User> mapEachRow =  (ResultSet resultSet, int i) -> {
-        Long userId = resultSet.getLong("user_id");
-        String  userName = resultSet.getString("user_name");
-        String encryptedPassword = resultSet.getString("encryted_password");
-
-        return new User(userId,userName, encryptedPassword);
-    };
+    RowMapper<User> mapEachRow = (ResultSet resultSet, int i) ->
+            new User(resultSet.getLong("user_id"), resultSet.getString("user_name"), resultSet.getString("encryted_password"));
 }
